d5d53cc2dc2f390b775e21ff6234790df7862f36
raulmahya123 <raulmahya11@gmail.com>
2022-09-20 21:51:52 +0700

    testing guys

---
+++ fundamental/fundamental/Program.cs
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        printf("%i ", i * 3);//print kelipatan 3
+        i++;
+    }
+    while (i <= 5);
+    return 0;
+}
---
+++ testing/testing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            //input data
+
+            for(int k=0; k<10; k++)
+            {
+                Console.WriteLine("halo raul" + k);
+                a[k] = Convert.ToInt32(Console.ReadLine());
+            }
+            //kebalikan dari inpudata dan menamilkan data yang di atas
+            for (int k = 0; k < 10; k++)
+            {
+                Console.WriteLine("\n"); //spasi
+                Console.WriteLine("isi array ke -" + k);
+                Console.WriteLine(a[k]);
+            }
+
+        }
+    }
+}
